6569e725170b0b70a17b64c62b8f0b8e230739e3
KarlSherman1999 <karlsherman1999@gmail.com>
2022-02-01 20:14:57 +0000

    bugfix

--- src/commands/private-message.js
+++ src/commands/private-message.js
@@ -1,7 +1,6 @@
 module.exports = (client,triggerText,replyText) =>{
     client.on('message', msg => {
         if(
-            msg.channel.type === 'dm' &&
             msg.content.toLowerCase() === triggerText.toLowerCase()
             ){
             msg.author.send(replyText)
